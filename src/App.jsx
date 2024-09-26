482a3a201643e886b77310cd5e8481845ac3e899
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-09-26 11:59:19 -0300

    ESTRUCTURANDO PORTFOLIO

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>
+        PORTFOLIO NICOLAS GABRIEL RAFFONAUD
+      </h1>
+    </>
+  );
+}
+
+export default App
